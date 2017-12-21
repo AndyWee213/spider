2220f5abf7e5661d9b991567c4f009fe60120c65
andy wee <qiangw808@gmail.com>
2017-12-21 08:44:44 +0800

    add detail

--- global_constant.py
+++ global_constant.py
@@ -2,8 +2,8 @@
 # encoding': ' utf-8
 
 counties_of_hangzhou = {
-    'xihu': '西湖', 'xiacheng': '下城', 'jianggan': '江干', 'gongshu': '拱墅', 'shangcheng': '上城', 'binjiang': '滨江',
-    'yuhang': '余杭', 'xiaoshan': '萧山',
+    'xiaoshan': '萧山',
     'xiasha': '下沙'}
 
-counties_of_test = {'xiacheng': '下城'}
+counties_of_test = {'yuhang': '余杭', 'xihu': '西湖', 'xiacheng': '下城', 'jianggan': '江干', 'gongshu': '拱墅', 'shangcheng': '上城',
+                    'binjiang': '滨江'}
--- lianjia_community.py
+++ lianjia_community.py
@@ -7,17 +7,55 @@ from bs4 import BeautifulSoup
 from openpyxl import Workbook
 import global_constant
 
+header = {
+    'Host': "hz.lianjia.com",
+    'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    'Accept-Encoding': "gzip, deflate, sdch",
+    'Accept-Language': "zh-CN,zh;q=0.8",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36',
+    'Connection': "keep-alive"}
+
 
 def get_content(html, sheet, row_number_excel):
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'lxml')
     community_list = soup.find_all('li', attrs={'class': 'xiaoquListItem'})
 
     for item in community_list:
         row_number_excel = row_number_excel + 1
         name_location = 'A%s' % row_number_excel
         price_location = 'B%s' % row_number_excel
+        time_location = 'C%s' % row_number_excel
+        type_location = 'D%s' % row_number_excel
+        fee_location = 'E%s' % row_number_excel
+        service_company_location = 'F%s' % row_number_excel
+        building_company_location = 'G%s' % row_number_excel
+        building_count = 'H%s' % row_number_excel
+        room_count = 'I%s' % row_number_excel
         sheet[name_location] = item.find('div', attrs={'class': 'title'}).text
         sheet[price_location] = item.find('div', attrs={'class': 'totalPrice'}).text
+
+        detail_url_prefix = 'https://hz.lianjia.com/xiaoqu/'
+        community_id = item['data-id']
+        detail_url = detail_url_prefix + community_id
+        detail_soup = BeautifulSoup(get_html(detail_url), 'lxml')
+        community_info_list = detail_soup.find_all('div', attrs={'class': 'xiaoquInfoItem'})
+        for community_item in community_info_list:
+            label = community_item.find('span', attrs={'class': 'xiaoquInfoLabel'}).text
+            content = community_item.find('span', attrs={'class': 'xiaoquInfoContent'}).text
+            if label == '建筑年代':
+                sheet[time_location] = content
+            elif label == '建筑类型':
+                sheet[type_location] = content
+            elif label == '物业费用':
+                sheet[fee_location] = content
+            elif label == '物业公司':
+                sheet[service_company_location] = content
+            elif label == '开发商':
+                sheet[building_company_location] = content
+            elif label == '楼栋总数':
+                sheet[building_count] = content
+            elif label == '房屋总数':
+                sheet[room_count] = content
 
     next_page_div = soup.find('div', attrs={'class': 'contentBottom'})
     page_data = next_page_div.find('div', attrs={'class': 'house-lst-page-box'})['page-data']
@@ -26,19 +64,16 @@ def get_content(html, sheet, row_number_excel):
     return current_page_num != total_page and int(current_page_num) + 1 or None, row_number_excel
 
 
-def get_html(url_prefix, item, page_num, sheet, row_number_excel):
-    header = {
-        'Host': "hz.lianjia.com",
-        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        'Accept-Encoding': "gzip, deflate, sdch",
-        'Accept-Language': "zh-CN,zh;q=0.8",
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36',
-        'Connection': "keep-alive"}
+def get_list_html(url_prefix, item, page_num, sheet, row_number_excel):
     while page_num is not None:
         print('开始获取' + item[1] + '区小区列表第' + str(page_num) + '页信息')
         url = url_prefix + item[0] + '/pg' + str(page_num)
-        html = requests.get(url, headers=header).content.decode('utf-8')
+        html = get_html(url)
         page_num, row_number_excel = get_content(html, sheet, row_number_excel)
+
+
+def get_html(url):
+    return requests.get(url, headers=header).content.decode('utf-8')
 
 
 def get_communities_of_hangzhou():
@@ -48,7 +83,7 @@ def get_communities_of_hangzhou():
         ws = wb.active
         ws.title = item[1]
         url_prefix = 'https://hz.lianjia.com/xiaoqu/'
-        get_html(url_prefix, item, 1, ws, 0)
+        get_list_html(url_prefix, item, 1, ws, 0)
         wb.save(filename=excel_name)
 
 
